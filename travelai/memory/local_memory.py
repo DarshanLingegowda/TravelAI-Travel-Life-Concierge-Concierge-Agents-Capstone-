1487953be73a02db935b553f5b0c8fcbddf5df2e
Darshan <darshanl1711@gmail.com>
2025-11-25 20:49:18 +0530

    Update local_memory.py

--- travelai/memory/local_memory.py
+++ travelai/memory/local_memory.py
@@ -1,1 +1,10 @@
 
+class LocalMemory:
+    def __init__(self):
+        self.store = {}
+
+    def save(self, key: str, value: str):
+        self.store[key] = value
+
+    def get(self, key: str):
+        return self.store.get(key)
